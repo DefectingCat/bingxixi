7ff5b42455ce45c8637c7661d2c734a7b56eff72
xfy <i@rua.plus>
2025-09-22 14:21:34 +0800

    feat(mms): handle mms login
    save cookies

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,4 +1,6 @@
-use std::sync::{LazyLock, Mutex};
+use std::{sync::LazyLock, time::Duration};
+
+use tokio::sync::Mutex;
 
 use crate::AppState;
 
@@ -7,7 +9,22 @@ pub fn platform(state: tauri::State<AppState>) -> &'static str {
     state.lock().map(|s| s.platform).unwrap_or("unknown")
 }
 
+/// 登录到 mms 的 webview 窗口
 static MMS: LazyLock<Mutex<Option<tauri::WebviewWindow>>> = LazyLock::new(|| Mutex::new(None));
+
+/// mms 的全局状态
+pub struct MmsStore {
+    /// 是否已经登录
+    pub logged: bool,
+    /// cookie
+    pub cookie: Vec<tauri::webview::Cookie<'static>>,
+}
+static MMS_STORE: LazyLock<Mutex<MmsStore>> = LazyLock::new(|| {
+    Mutex::new(MmsStore {
+        logged: false,
+        cookie: vec![],
+    })
+});
 
 fn init_mms_window(window: &tauri::WebviewWindow) -> Result<(), String> {
     window
@@ -40,15 +57,11 @@ fn init_mms_window(window: &tauri::WebviewWindow) -> Result<(), String> {
                 console.log('onload init');
                 const invoke = window.__TAURI__.core.invoke;
                 console.log(invoke);
-                const btn = createBtn('完成登录');
                 const btn1 = createBtn('关闭窗口');
                 const div = createDiv();
                 div.appendChild(btn);
                 div.appendChild(btn1);
 
-                btn.onclick = async function() {
-                    await invoke('logged_mms');
-                };
                 btn1.onclick = async function() {
                     await invoke('destory_mms');
                 };
@@ -63,7 +76,7 @@ fn init_mms_window(window: &tauri::WebviewWindow) -> Result<(), String> {
 /// 首先检查是否已经创建了 webview 窗口，如果没有则创建
 /// 如果已经创建了，再刷新
 pub async fn login_mms(app: tauri::AppHandle) -> Result<(), String> {
-    let window = MMS.lock().map_err(|e| e.to_string())?.take();
+    let window = MMS.lock().await.take();
     let window = if let Some(window) = window {
         window.reload().map_err(|e| e.to_string())?;
         window.show().map_err(|e| e.to_string())?;
@@ -73,7 +86,7 @@ pub async fn login_mms(app: tauri::AppHandle) -> Result<(), String> {
             &app,
             "login_mms",
             tauri::WebviewUrl::External(
-                "https://mms.pinduoduo.com/"
+                "https://mms.pinduoduo.com/login/"
                     .parse()
                     .map_err(|_| "invalid url")?,
             ),
@@ -86,16 +99,40 @@ pub async fn login_mms(app: tauri::AppHandle) -> Result<(), String> {
         window
     };
 
+    // 初始化 mms 的窗口，添加关闭按钮
     init_mms_window(&window)?;
     window.open_devtools();
 
-    *MMS.lock().map_err(|e| e.to_string())? = Some(window);
+    // 将 mms 的窗口存储到全局状态，已供其他命令使用
+    *MMS.lock().await = Some(window);
+
+    // 监听 mms 的窗口，当登录成功后，关闭窗口
+    tauri::async_runtime::spawn(async move {
+        let mut interval = tokio::time::interval(Duration::from_secs(1));
+        loop {
+            interval.tick().await;
+            let window = MMS.lock().await;
+            let window = window.as_ref();
+            let Some(window) = window else {
+                break;
+            };
+            let url = window.url()?;
+            if !url.path().contains("login") {
+                let mut mms_store = MMS_STORE.lock().await;
+                mms_store.logged = true;
+                mms_store.cookie = window.cookies()?;
+                window.destroy()?;
+                break;
+            }
+        }
+        anyhow::Ok(())
+    });
     Ok(())
 }
 
 #[tauri::command]
 pub async fn hide_mms() -> Result<(), String> {
-    let window = MMS.lock().map_err(|e| e.to_string())?.take();
+    let window = MMS.lock().await.take();
     let Some(window) = window else {
         return Err("mms window not found".to_string());
     };
@@ -103,13 +140,13 @@ pub async fn hide_mms() -> Result<(), String> {
     window.hide().map_err(|e| e.to_string())?;
     window.close_devtools();
 
-    *MMS.lock().map_err(|e| e.to_string())? = Some(window);
+    *MMS.lock().await = Some(window);
     Ok(())
 }
 
 #[tauri::command]
 pub async fn destory_mms() -> Result<(), String> {
-    let window = MMS.lock().map_err(|e| e.to_string())?.take();
+    let window = MMS.lock().await.take();
     let Some(window) = window else {
         return Err("mms window not found".to_string());
     };
@@ -117,13 +154,13 @@ pub async fn destory_mms() -> Result<(), String> {
     window.destroy().map_err(|e| e.to_string())?;
     window.close_devtools();
 
-    *MMS.lock().map_err(|e| e.to_string())? = None;
+    *MMS.lock().await = None;
     Ok(())
 }
 
 #[tauri::command]
 pub async fn logged_mms() -> Result<(), String> {
-    let window = MMS.lock().map_err(|e| e.to_string())?.take();
+    let window = MMS.lock().await.take();
     let Some(window) = window else {
         return Err("mms window not found".to_string());
     };
@@ -136,6 +173,6 @@ pub async fn logged_mms() -> Result<(), String> {
     window.destroy().map_err(|e| e.to_string())?;
     window.close_devtools();
 
-    *MMS.lock().map_err(|e| e.to_string())? = None;
+    *MMS.lock().await = None;
     Ok(())
 }
