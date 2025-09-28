635a496f774c555365bdb109415beadea73e6bb0
xfy <i@rua.plus>
2025-09-28 17:11:41 +0800

    feat(app): add system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,13 @@
 use std::sync::Mutex;
 
+use anyhow::anyhow;
 use serde_json::json;
+use tauri::menu::Menu;
+use tauri::menu::MenuItem;
+use tauri::tray::MouseButton;
+use tauri::tray::MouseButtonState;
+use tauri::tray::TrayIconBuilder;
+use tauri::tray::TrayIconEvent;
 use tauri::Manager;
 use tauri_plugin_store::StoreExt;
 
@@ -22,6 +29,7 @@ pub static MMS_STORE_KEY: &str = "mms";
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
             let platform = if cfg!(target_os = "windows") {
@@ -47,6 +55,46 @@ pub fn run() {
                     *mms_store = mms_store_value;
                 });
             }
+
+            // system tray
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .show_menu_on_left_click(false)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    "test" => {
+                        println!("test");
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        // in this example, let's show and focus the main window when the tray is clicked
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                })
+                .icon(
+                    app.default_window_icon()
+                        .ok_or(anyhow!("default_window_icon"))?
+                        .clone(),
+                )
+                .build(app)?;
+
             Ok(())
         })
         .on_window_event(|window, event| match event {
